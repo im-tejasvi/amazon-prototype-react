976c5259c074a5fa27c600871bc9e00bba231396
Tejasvi Sharma <imtejasvi@gmail.com>
2022-05-06 15:33:31 +0530

    Added Home Page and initialized routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="app">
-      <Header />
-      {/* <Home /> */}
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Product.js
@@ -0,0 +1,29 @@
+import './Product.css';
+import React from 'react';
+
+const Product = ({ title, image, price, rating }) => {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+      </div>
+
+      <img src={image} alt="" />
+
+      <button>Add to Basket</button>
+    </div>
+  );
+};
+
+export default Product;
