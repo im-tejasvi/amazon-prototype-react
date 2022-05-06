418dbccd772972f129d8acb260b8f137028da1b5
Tejasvi Sharma <imtejasvi@gmail.com>
2022-05-06 22:59:04 +0530

    Implemented baket state store

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,30 @@
 import './Product.css';
 import React from 'react';
+import { useStateValue } from '../store/StateProvider';
 
-const Product = ({ title, image, price, rating }) => {
+const Product = ({ id, title, image, price, rating }) => {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log('basket >>>', basket);
+
+  const addToBasket = () => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
           {Array(rating)
@@ -21,9 +37,13 @@ const Product = ({ title, image, price, rating }) => {
 
       <img src={image} alt="" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
+};
+
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 7);
 };
 
 export default Product;
