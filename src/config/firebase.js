3e8a9a9eb6b878d14c3cd5ffbf97a5fe38b14212
Tejasvi Sharma <imtejasvi@gmail.com>
2022-05-06 12:33:49 +0530

    Added Header Layout

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCRDYWs5jA4F0Xd3wpWnaMq6Zm-rDYPKIc',
+  authDomain: 'prototype-react-974ba.firebaseapp.com',
+  projectId: 'prototype-react-974ba',
+  storageBucket: 'prototype-react-974ba.appspot.com',
+  messagingSenderId: '816695121946',
+  appId: '1:816695121946:web:f3e130a85a2a63daea59a3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
